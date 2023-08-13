00953b199062220b67053aceffec105a3a41ffaf
dacoaster <david@yatt.ai>
2023-08-13 09:52:13 -0700

    Add roles to accelerators

--- src/menu.js
+++ src/menu.js
@@ -74,13 +74,13 @@ const createMenu = (win) => {
     {
       label: i18n.t("menu.edit"),
       submenu: [
-        { label: i18n.t("menu.undo"), accelerator: "CmdOrCtrl+Z" },
-        { label: i18n.t("menu.redo"), accelerator: "CmdOrCtrl+Y" },
+        { label: i18n.t("menu.undo"), accelerator: "CmdOrCtrl+Z", role: "undo" },
+        { label: i18n.t("menu.redo"), accelerator: "CmdOrCtrl+Y", role: "redo" },
         { type: "separator" },
-        { label: i18n.t("menu.cut"), accelerator: "CmdOrCtrl+X" },
-        { label: i18n.t("menu.copy"), accelerator: "CmdOrCtrl+C" },
-        { label: i18n.t("menu.paste"), accelerator: "CmdOrCtrl+V" },
-        { label: i18n.t("menu.select_all"), accelerator: "CmdOrCtrl+A" },
+        { label: i18n.t("menu.cut"), accelerator: "CmdOrCtrl+X", role: "cut" },
+        { label: i18n.t("menu.copy"), accelerator: "CmdOrCtrl+C", role: "copy" },
+        { label: i18n.t("menu.paste"), accelerator: "CmdOrCtrl+V", role: "paste" },
+        { label: i18n.t("menu.select_all"), accelerator: "CmdOrCtrl+A", role: "selectAll" },
       ],
     },
     {
