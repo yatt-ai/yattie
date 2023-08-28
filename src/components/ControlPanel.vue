950cf36738404e91a073ed57082f482460d08202
dacoaster <david@yatt.ai>
2023-08-28 06:31:08 -0700

    Missed reference

--- src/components/ControlPanel.vue
+++ src/components/ControlPanel.vue
@@ -755,7 +755,7 @@ export default {
     DurationConfirmDialog,
     AudioErrorDialog,
     EndSessionDialog,
-    MinimizeControlWrapper,
+    //MinimizeControlWrapper,
     JiraExportSession,
     TestRailExportSession,
     JiraAddIssue,
